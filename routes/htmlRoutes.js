5db912bcf07d4142bba6b4d5822c01a985bd6d92
nikki <rk_787@hotmail.co.uk>
2020-12-02 18:38:55 +0000

    set up htmlroutes file and connect

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+const path = require("path"); 
+const router = require("express").Router();
+
+//Set up all/ router to return index.html file and display to user
+
+
+
+//Set Up /notes router to return and display notes.html file to user
+
+
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes")
 
 //Initialise the application and set up a port for it to run
 
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
+app.use("/", htmlRoutes)
 
 // Start the server
 app.listen(PORT, () => console.log("application running on port " + PORT));

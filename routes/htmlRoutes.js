8cb1445c2afb51b3ebf4d7d9402ca0314008ebd2
nikki <rk_787@hotmail.co.uk>
2020-12-02 18:43:14 +0000

    Set up index.html to display on / route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,10 @@ const path = require("path");
 const router = require("express").Router();
 
 //Set up all/ router to return index.html file and display to user
-
+router.get("/", (req, res) => {
+    const htmlFilePath = path.join(__dirname, "../public/index.html");
+    res.sendFile(htmlFilePath)
+})
 
 
 //Set Up /notes router to return and display notes.html file to user
